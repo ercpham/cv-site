6ee3e19988cf94b759ff1d7a4469b2316e8707ed
ercpham <erc.pham@gmail.com>
2020-12-22 14:15:33 -0800

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [response, setResponse] = useState('');
+  const [post, setPost] = useState('');
+  const [responseToPost, setResponseToPost] = useState('');
+
+  useEffect(() => {
+    callApi()
+      .then(res => setResponse(res.express))
+      .catch(err => console.log(err));
+  });
+
+  const callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: post }),
+    });
+    const body = await response.text();
+
+    setResponseToPost(body);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <p>{response}</p>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <strong>Post to Server:</strong>
+        </p>
+        <input
+          type="text"
+          value={post}
+          onChange={e => setPost(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{responseToPost}</p>
+    </div>
+  );
+}
+
+export default App;
